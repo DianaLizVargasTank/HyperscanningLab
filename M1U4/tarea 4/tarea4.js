c410ac9ab0b419f8f4505e3519a352afbd0062f5
Diana Liz Vargas <diana.lizv@gmail.com>
2022-07-26 02:53:36 -0400

    agrega todos los archivos del maquetado

--- M1U4/tarea 4/tarea4.js
+++ M1U4/tarea 4/tarea4.js
@@ -1,3 +1,11 @@
+// ejercicio 1 Utilizando lo aprendido escribir el código necesario para que dada una distancia determine el medio de transporte apropiado de acuerdo a las siguientes reglas:
+
+// 0 a 1000 metros = pie
+//1000 a 10000 metros = bicicleta
+// 10000 a 30000 metros = colectivo
+// 30000 a 100000 metros = auto
+// +100000 = avión
+
 let nombre = prompt('¿Cuál es tu nombre?');
 let metros = prompt("Ingresa en metros la distancia que debes recorrer");
 let transporte = '';
@@ -14,3 +22,5 @@ if (metros > 0 && metros <= 1000) {transporte = 'pie';
 alert (nombre + ' te recomiendo que viajes en '+ transporte)
 document.write('Hola '+ nombre +', ¿cómo va todo? ')
 document.write ('Me dijiste tenías que viajar '+ metros + ' metros' + ' te recomiendo entonces ir en '+ transporte)
+
+// ejercicio 2 Haciendo uso de los bucles, recorrer un array de números y determinar cual es el mayor 
